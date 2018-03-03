14f15a35830bba6301918deb2bae8cf1308f0251
何建军103 <hejianjun11795@foxmail.com>
2018-03-03 19:48:33 +0800

    修改为阿里仓库

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 npm --python=/usr/alibaba/install/python-3.5.0/bin/python3 --registry=https://registry.npm.taobao.org install
 npm --registry=https://registry.npm.taobao.org install hexo-cli
-hexo deploy -g
+npm run hexo deploy -g
