175acfa521cd0ac37336708fcf99cf448ec09e72
hejianjun <hejianjun@thunisoft.com>
2018-03-05 13:22:20 +0800

    debug

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 npm --python=/usr/alibaba/install/python-3.5.0/bin/python3 --registry=https://registry.npm.taobao.org install
-npm --registry=http://r.cnpmjs.org/ install hexo-cli
+npm install -g hexo-cli --registry http://r.cnpmjs.org/
 hexo deploy -g
